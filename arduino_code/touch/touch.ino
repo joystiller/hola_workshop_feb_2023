e2d89e844a33f6fb5f09be8b7c5f7f96878f5025
kaffeocigg <antonsoederberg@gmail.com>
2023-02-21 11:37:50 +0100

    changes to some pd patches

--- arduino_code/touch/touch.ino
+++ arduino_code/touch/touch.ino
@@ -1,9 +1,52 @@
+#include <CapacitiveSensor.h>
+
+// if serial monitor moves really slowly, it's probably due to a faulty cable or broken connection somewhere.
+
+/*
+CapacitiveSensor cs0 = CapacitiveSensor(2, 3);
+CapacitiveSensor cs1 = CapacitiveSensor(4, 5);
+CapacitiveSensor cs2 = CapacitiveSensor(6, 7);
+
 void setup()
 {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  long measurement = cs0.capacitiveSensor(30);
+  long measurement2 = cs1.capacitiveSensor(30);
+  long measurement3 = cs2.capacitiveSensor(30);
+  Serial.println(measurement);
+  Serial.println(measurement2);
+  Serial.println(measurement3);
+  delay(10);
+}
+*/
+
+// Undersök sen hur man ska loopa igenom alla sensorer. Det går ju inte om man inte först skapar en array med alla sensorer. Fråga chatGPT om en lösning.
+
+// Multiple sensors
+CapacitiveSensor cs[3] = {
+    CapacitiveSensor(2, 3),
+    CapacitiveSensor(4, 5),
+    CapacitiveSensor(6, 7)};
+
+void setup()
+{
+  Serial.begin(9600);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+  // Read digital inputs one by one and send them to Pd
+  for (int i = 0; i < 3; i++)
+  {
+    long val = cs[i].capacitiveSensor(30);
+    Serial.print(val);
+    Serial.print(" ");
+  }
+  Serial.println();
+  delay(10);
 }

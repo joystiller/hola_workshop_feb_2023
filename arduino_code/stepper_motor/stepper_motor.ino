f2a4fa5df55bdb386f56dca4ebce52122e86c434
kaffeocigg <antonsoederberg@gmail.com>
2023-02-22 21:05:05 +0100

    added stepper motor, not finished

--- arduino_code/stepper_motor/stepper_motor.ino
+++ arduino_code/stepper_motor/stepper_motor.ino
@@ -1,3 +1,9 @@
+#include <Stepper.h>
+
+const float STEPS_PER_REVOLUTION = 32;
+
+Stepper steppermotor(STEPS_PER_REVOLUTION, 8, 9, 10, 11);
+
 void setup()
 {
   // put your setup code here, to run once:
